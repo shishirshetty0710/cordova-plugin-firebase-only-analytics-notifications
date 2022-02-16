d85f15bccc80b99bd213ddf184d5ad9321960c35
DavidSousa <davidbdsousa@gmail.com>
2022-02-16 09:26:17 +0000

    Update FirebasePluginMessagingService.java

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.core.app.TaskStackBuilder;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
